9f60b06e7900e178f1338a7abecde527e642c52c
Matthew Cotton <matt@thecottons.com>
2016-07-28 22:20:53 -0400

    'Initial commit (auto)'

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Auto-generated setup.py file.
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='findtime',
+    version=open('VERSION.txt').read(),
+    author='Matthew Cotton',
+    author_email='matt@thecottons.com',
+    packages=['findtime'],
+    # py_modules=[],
+    # scripts=[],
+    url='http://pypi.python.org/pypi/findtime/',
+    license='MIT - LICENSE.txt',
+    description='A tool for concisely describing schedules and finding mutual free time!',
+    long_description=open('README.rst').read(),
+    install_requires=[],
+)
